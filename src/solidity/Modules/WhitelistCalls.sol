df366e4e87b77d51b2b09bbb6a758569ce04e33c
Corban Villa <corbanvilla@gmail.com>
2022-03-11 23:13:53 +0400

    started whitelist contract

---
+++ src/solidity/Modules/WhitelistCalls.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+/**
+ * @dev Whitelist
+ *
+ */
+contract WhitelistCalls {
+
+    function 
+
+}
