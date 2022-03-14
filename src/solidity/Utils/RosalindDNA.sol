6e37b4e181df7c55c77c39e5f15cb812d56f9d7e
Corban Villa <corbanvilla@gmail.com>
2022-03-14 18:03:59 +0400

    dna mutation refactoring / optimizations

--- src/solidity/Utils/RosalindDNA.sol
+++ src/solidity/Utils/RosalindDNA.sol
@@ -26,26 +26,19 @@ library RosalindDNA {
         uint256 randomSeed
     ) internal pure returns (uint256 childDNA) {
         // Loop genes
-        for (uint geneIdx = 0; geneIdx < genes.length; geneIdx++) {
+        for (uint16 geneIdx = 0; geneIdx < genes.length; geneIdx++) {
 
             // Gene details
-            uint256 geneStartIdx = genes[geneIdx];
-            uint256 geneEndIdx;
-            if (geneIdx < genes.length-1)
-                geneEndIdx = genes[geneIdx+1];
-            else
-                geneEndIdx = 256;
+            uint16 geneStartIdx = genes[geneIdx];
+            uint16 geneEndIdx = geneIdx < genes.length-1? genes[geneIdx+1] : 256;
 
             // Select parent
             uint8 randomParentIdx = uint8(SourceRandom.getSeededRandom(randomSeed, geneIdx) % parents.length);
             uint256 selectedParent = parents[randomParentIdx];
 
-            // Bitmasking
-            uint256 bitMaskStart = type(uint256).max << geneStartIdx;
-            uint256 bitMaskEnd = type(uint256).max >> (256 - geneEndIdx);
-            uint256 bitMask = bitMaskStart & bitMaskEnd;
             // Isolate our gene
-            uint256 gene = selectedParent & bitMask;
+            uint256 bitmask = get256Bitmask(geneStartIdx, geneEndIdx);
+            uint256 gene = selectedParent & bitmask;
 
             // Save genes to childDNA
             childDNA = childDNA | gene;
@@ -81,41 +74,79 @@ library RosalindDNA {
         uint256 randomSeed,
         uint256[] memory mutationRates
     ) internal pure returns (uint256 childDNA) {
-        // Loop genes
+
+        // Breed normally
+        childDNA = breedDNASimple(parents, genes, randomSeed);
+
+        // Add mutations
+        childDNA = generateMutations(childDNA, genes, randomSeed, mutationRates);
+
+    }
+
+    /**
+     * @dev Mutates encoded DNA
+     * @param dna existing DNA to mutate
+     * @param genes start indicies of each gene.
+     * First index should ALWAYS be 0. Using [0, 128] splits
+     * the DNA into two genes of equal length
+     * @param randomSeed random value to use for gene splicing
+     * @param mutationRates probability that a random gene is picked vs. mutated.
+     * A higher mutation rate means a higher probability of having a random gene.
+     * The mutation rate m is a probability of m/(2^256-1) or in other words the
+     * probability that geneRandomnessSeed <= m. We therefore assign a mutated
+     * gene at the following rates according to m:
+     * 0 = 000... => 0%
+     * 2^254-1 = 001... => 25%
+     * 2^255-1 = 011... => 50%
+     * 2^255 + 2^254 -1 => 75%
+     * 2^256-1 = 111... => 100% Always mutate
+     * Calculated probability as a function: 1/2^(256-m)
+     * @return mutatedDNA combined child DNA with mutations occuring.
+     */
+    function generateMutations(
+        uint256 dna,
+        uint8[] memory genes,
+        uint256 randomSeed,
+        uint256[] memory mutationRates
+    ) internal pure returns (uint256 mutatedDNA) {
+        // Start with empty mask (no mutations)
+        uint256 mutationMask;
+
+        // Loop genes, create a mutation mask
         for (uint geneIdx = 0; geneIdx < genes.length; geneIdx++) {
-
-            // Gene details
-            uint256 geneStartIdx = genes[geneIdx];
-            uint256 geneEndIdx;
-            if (geneIdx < genes.length-1)
-                geneEndIdx = genes[geneIdx+1];
-            else
-                geneEndIdx = 256;
-
-            // Select parent / mutation
-            uint256 selectedGene;
+            // Decide if we want to mutate
             uint256 geneMutationSeed = SourceRandom.getSeededRandom(randomSeed, geneIdx);
-            if (geneMutationSeed <= mutationRates[geneIdx])
-                // Mutate Gene
-                selectedGene = SourceRandom.getSeededRandom(geneMutationSeed, 0);
-            else
-                // Select random parent
-                selectedGene = parents[  // parents[randomParentIdx]
-                    uint8(SourceRandom.getSeededRandom(randomSeed, geneIdx) % parents.length)
-                ];
+            if (geneMutationSeed > mutationRates[geneIdx])
+                // Skip to the next gene if we shouldn't mutate
+                continue;
 
-            // Bitmasking
-            uint256 bitMaskStart = type(uint256).max << geneStartIdx;
-            uint256 bitMaskEnd = type(uint256).max >> (256 - geneEndIdx);
-            uint256 bitMask = bitMaskStart & bitMaskEnd;
-            // Isolate our gene
-            uint256 gene = selectedGene & bitMask;
+            // Note our mutation in mutationMask
+            uint16 geneStartIdx = genes[geneIdx];
+            uint16 geneEndIdx = geneIdx < genes.length-1? genes[geneIdx+1] : 255;
 
-            // Save genes to childDNA
-            childDNA = childDNA | gene;
+            uint256 bitmask = get256Bitmask(geneStartIdx, geneEndIdx);
+            mutationMask = mutationMask | bitmask;
         }
 
-        return childDNA;
+        // Source new mutations from our existing dna + random seed, then merge
+        uint256 mutations = SourceRandom.getSeededRandom(dna, randomSeed);
+        mutatedDNA = mutations & mutationMask;
+
+        // Merge existing DNA after slicing out mutation genes
+        dna = dna & (~mutationMask);
+        mutatedDNA = dna | mutatedDNA;
+    }
+
+    /**
+     * @dev Generates a 256-bit bitmask from startBit:endBit
+     * @param startBit beginning of mask
+     * @param endBit end of mask
+     * @return bitMask combined bitmask
+     */
+    function get256Bitmask(uint16 startBit, uint16 endBit) internal pure returns(uint256 bitMask) {
+        uint256 bitMaskStart = type(uint256).max << startBit;
+        uint256 bitMaskEnd = type(uint256).max >> (256 - endBit);
+        bitMask = bitMaskStart & bitMaskEnd;
     }
 
 }
