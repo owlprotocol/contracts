8689050514b3c39427dea00550cb4046aec8391c
Corban Villa <corbanvilla@gmail.com>
2022-04-19 18:39:11 +0400

    rebase continued

--- src/metadata/metadata/index.ts
+++ src/metadata/metadata/index.ts
@@ -1,2 +1,2 @@
 export { default as SpecieTrait } from './SpecieTrait';
-export { default as SpecieMetadata, Metadata, validateSchema, validateAndGetSchema } from './SpecieMetadata';
+export { default as SpecieMetadata, validateSchema, validateAndGetSchema } from './SpecieMetadata';
