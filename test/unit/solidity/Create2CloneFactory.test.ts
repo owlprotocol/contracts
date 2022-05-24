962703c4ba9f03b8546b9f6a6f61e30da725d6bd
HrikB <hrik.bhowal@gmail.com>
2022-05-24 16:10:12 -0700

    interacting with proxies test

--- test/unit/solidity/Create2CloneFactory.test.ts
+++ test/unit/solidity/Create2CloneFactory.test.ts
@@ -4,8 +4,8 @@ import {
     Create2CloneFactory,
     ERC721MintableCode__factory,
     ERC721MintableCode,
-    // ERC721Mintable,
-    // ERC721Mintable__factory,
+    ERC721Mintable,
+    ERC721Mintable__factory,
 } from '../../../typechain';
 import { abi } from '../../../artifacts/contracts/ERC721/ERC721MintableCode.sol/ERC721MintableCode.json';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
@@ -22,18 +22,18 @@ describe('Create2CloneFactory.sol', () => {
 
     let accounts;
     let admin: SignerWithAddress;
-    // let minter1: SignerWithAddress;
-    // let minter2: SignerWithAddress;
+    let minter1: SignerWithAddress;
+    let minter2: SignerWithAddress;
 
-    // let nftContrFactory: ERC721Mintable__factory;
-    // let ducksContr: ERC721Mintable;
-    // let robotsContr: ERC721Mintable;
+    let nftContrFactory: ERC721Mintable__factory;
+    let ducksContr: ERC721Mintable;
+    let robotsContr: ERC721Mintable;
     let ducksContrAddr: string;
     let robotsContrAddr: string;
     before(async () => {
         accounts = await ethers.getSigners();
-        [admin] = accounts;
-        // nftContrFactory = await ethers.getContractFactory('ERC721Mintable');
+        [admin, minter1, minter2] = accounts;
+        nftContrFactory = await ethers.getContractFactory('ERC721Mintable');
 
         //deploy ERC721 code
         nftCodeFactory = await ethers.getContractFactory('ERC721MintableCode');
@@ -73,5 +73,25 @@ describe('Create2CloneFactory.sol', () => {
         const event2 = events2 ? events2.filter((e) => e.event === 'NewClone')[0] : undefined;
         robotsContrAddr = event2?.args?.proxyAddr;
         console.log('Robot proxy deployed to', robotsContrAddr);
+        expect(event2).to.not.equal(undefined); //NewClone event emitted
+        expect(robotsContrAddr).to.equal(
+            await create2CloneContr.getCloneAddr(create2CloneContr.address, nftCode.address, SALT2),
+        );
+    });
+
+    it('mint from proxies', async () => {
+        ducksContr = await nftContrFactory.attach(ducksContrAddr);
+        robotsContr = await nftContrFactory.attach(robotsContrAddr);
+
+        const mintDuck = await ducksContr.connect(admin).mint(minter1.address, 13);
+        const mintRobot = await robotsContr.connect(admin).mint(minter2.address, 18);
+        await mintDuck.wait();
+        await mintRobot.wait();
+
+        expect(await ducksContr.ownerOf(13)).to.equal(minter1.address);
+        expect(await robotsContr.ownerOf(18)).to.equal(minter2.address);
+        await expect(ducksContr.connect(minter1).mint(minter1.address, 19)).to.be.revertedWith(
+            `AccessControl: account ${minter1.address.toLowerCase()} is missing role 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6`,
+        );
     });
 });
