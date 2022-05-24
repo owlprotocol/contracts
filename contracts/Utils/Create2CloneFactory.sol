97a7356d004e3e0c843b0575bc73275b3a63567f
HrikB <hrik.bhowal@gmail.com>
2022-05-24 15:29:32 -0700

    proxy launch tests

--- contracts/Utils/Create2CloneFactory.sol
+++ contracts/Utils/Create2CloneFactory.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.0;
 
 contract Create2CloneFactory {
+    event NewClone(address proxyAddr, address logicAddr, bytes32 salt);
+
     /**
      * @dev Deploy an EIP-1167 proxy with CREATE2
-     * @param logic Address at which logic is stored
+     * @param logicAddr Address at which logic is stored
      * @param salt for CREATE2 deployment
      * @return result address of newly deployed clone
      */
-    function create2Clone(address logic, bytes32 salt) public returns (address result) {
-        bytes memory bytecode = getInitBytes(logic);
+    function create2Clone(address logicAddr, bytes32 salt) public returns (address result) {
+        bytes memory bytecode = getInitBytes(logicAddr);
 
         assembly {
             result := create2(
@@ -19,20 +21,23 @@ contract Create2CloneFactory {
                 salt
             )
         }
+
+        require(result != address(0), 'Create2CloneFactory: CREATE2 Failed to deploy');
+        emit NewClone(result, logicAddr, salt);
     }
 
     /**
      * @dev Deploy an EIP-1167 proxy with CREATE2 and call initializer function
-     * @param logic Address at which logic is stored
+     * @param logicAddr Address at which logic is stored
      * @param salt for CREATE2 deployment
      * @param data optional argument for calling initializer function on newly deployed contract.
      */
     function create2AtomicallyClone(
-        address logic,
+        address logicAddr,
         bytes32 salt,
         bytes memory data
     ) external returns (address result) {
-        result = create2Clone(logic, salt);
+        result = create2Clone(logicAddr, salt);
 
         //data is optional
         if (data.length > 0) {
@@ -43,17 +48,17 @@ contract Create2CloneFactory {
 
     /**
      * @dev Use logic adddress to generate EIP-1167 init code
-     * @param logic Address at which logic is stored
+     * @param logicAddr Address at which logic is stored
      * As per, https://medium.com/authereum/bytecode-and-init-code-and-runtime-code-oh-my-7bcd89065904
      * @return init The initial byte code for the address
      */
-    function getInitBytes(address logic) public pure returns (bytes memory) {
-        require(logic != address(0), 'Create2CloneFactory: Logic cannot be stored a the zero address');
+    function getInitBytes(address logicAddr) public pure returns (bytes memory) {
+        require(logicAddr != address(0), 'Create2CloneFactory: logicAddr cannot be stored a the zero address');
 
         return
             abi.encodePacked(
                 hex'3d602d80600a3d3981f3363d3d373d3d3d363d73',
-                bytes20(logic),
+                bytes20(logicAddr),
                 hex'5af43d82803e903d91602b57fd5bf3'
             );
     }
@@ -63,9 +68,17 @@ contract Create2CloneFactory {
      */
     function getCloneAddr(
         address deployer,
-        address logic,
+        address logicAddr,
         bytes32 salt
     ) external pure returns (address) {
-        return address(bytes20(keccak256(abi.encodePacked(hex'ff', deployer, salt, keccak256(getInitBytes(logic))))));
+        return
+            address(uint160(uint256(keccak256(abi.encodePacked(hex'ff', deployer, salt, getBytecodeHash(logicAddr))))));
+    }
+
+    /**
+     * @dev Utility function to get bytecode hash for CREATE2
+     */
+    function getBytecodeHash(address logicAddr) public pure returns (bytes32) {
+        return keccak256(getInitBytes(logicAddr));
     }
 }
