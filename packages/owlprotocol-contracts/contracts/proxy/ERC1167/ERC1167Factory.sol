ee9e3db660e16da0437dea042cb1b61b623b0d70
Leo Vigna <leo.vigna@gmail.com>
2022-05-28 17:46:11 +0400

    add ERC1167 proxy

---
+++ packages/owlprotocol-contracts/contracts/proxy/ERC1167/ERC1167Factory.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Clones} from '@openzeppelin/contracts-upgradeable/proxy/ClonesUpgradeable.sol';
+
+/**
+ * @dev EIP1167 Minimal Proxy Factory
+ */
+contract EIP1167Factory {
+    event NewClone(address instance, address implementation, bytes32 salt);
+
+    function clone(address implementation, bytes memory data) public returns (address instance) {
+        instance = Clones.clone(implementation);
+
+        //data is optional
+        if (data.length > 0) callInitializer(instance, data);
+
+        emit NewClone(instance, implementation, bytes32(0));
+    }
+
+    function cloneDeterministic(
+        address implementation,
+        bytes32 salt,
+        bytes memory data
+    ) public returns (address instance) {
+        //Salt init data
+        if (data.length > 0) salt = keccak25(abi.encodePacked(salt, data));
+
+        instance = Clones.cloneDeterministic(implementation, salt);
+
+        //data is optional
+        if (data.length > 0) callInitializer(instance, data);
+
+        emit NewClone(instance, implementation, salt);
+    }
+
+    function callInitializer(address instance, bytes memory data) internal {
+        (bool s, ) = instance.call(data);
+        require(s, 'Create2CloneFactory: Failed to call the proxy');
+    }
+
+    function predictDeterministicAddress(address implementation, bytes32 salt) public view returns (address predicted) {
+        return Clones.predictDeterministicAddress(implementation, salt);
+    }
+}
