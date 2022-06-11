30bc747855e02106e95d32d1a1defefa7949bd6e
HrikB <hrik.bhowal@gmail.com>
2022-06-11 10:07:24 +0400

    mints 1 billion by default not 1 million

---
+++ packages/owlprotocol-contracts/test/utils/createERC20.ts
@@ -0,0 +1,20 @@
+import { ethers } from 'hardhat';
+
+// Creates + returns dummy ERC20 tokens for use in testing
+export async function createERC20(tokens = 1) {
+    const mintAmount = 0; // 0 => mints 1billion to owner
+    const coinName = 'TESTCOIN';
+    const coinTicker = 'TST';
+    const FactoryERC20 = await ethers.getContractFactory('FactoryERC20');
+
+    const contracts = [];
+    for (let i = 0; i < tokens; i++) {
+        contracts.push(FactoryERC20.deploy(mintAmount, coinName, coinTicker));
+    }
+    const deployedContracts = await Promise.all(contracts);
+    // Assert all deployed
+    await Promise.all(deployedContracts.map((c) => c.deployed()));
+    return deployedContracts;
+}
+
+export default createERC20;
