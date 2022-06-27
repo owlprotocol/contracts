980da55e880e4610a5c07000adc373b754c80b0c
HrikB <hrik.bhowal@gmail.com>
2022-06-28 02:49:41 +0400

    MinterRandom.sol new architecture

--- packages/owlprotocol-contracts/contracts/plugins/Minter/builds/MinterRandom.sol
+++ packages/owlprotocol-contracts/contracts/plugins/Minter/builds/MinterRandom.sol
@@ -1,66 +1,105 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
+
 import '../MinterCore.sol';
-import './IMinterRandom.sol';
-import '../../Utils/SourceRandom.sol';
+import '../../../utils/SourceRandom.sol';
 
 /**
  * @dev Decentralized NFT Minter contract
  *
  */
-contract MinterRandom is MinterCore {
+contract MinterRandom is MinterCore, OwnableUpgradeable, UUPSUpgradeable {
     // Nonce
     uint256 private _numMinted;
 
-    // Events
-    event MintSpecies(uint256 indexed speciesId, address to, uint256 tokenId);
-
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
+    constructor() initializer {
+        _disableInitializers();
+    }
 
     // Constructor
-    function initialize() public override initializer {
+    function initialize(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) external initializer {}
+
+    function proxyInitialize(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) external onlyInitializing {}
+
+    function __MinterRandom_init(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) internal onlyInitializing {
+        __MinterRandom_init_unchained(_admin);
+        __MinterCore_init(_mintFeeToken, _mintFeeAddress, _mintFeeAmount, _nftContractAddr);
+
+        __Ownable_init();
+    }
+
+    function __MinterRandom_init_unchained(address _admin) internal onlyInitializing {
         // Register ERC1820 Private Interface
         bytes32 interfaceName = keccak256('OWLProtocol://MinterRandom');
         ERC1820ImplementerAuthorizeAll._registerInterfaceForAddress(interfaceName);
         // Register ERC165 Interface
         ERC165Storage._registerInterface(type(IMinterRandom).interfaceId);
+
+        _transferOwnership(_admin);
     }
 
     /**
      * @dev Create a new type of species and define attributes.
-     * @param speciesId address of associated NFT
-     * @return tokenId minted token id
      */
-    function mint(uint256 speciesId) public speciesExists(speciesId) returns (uint256 tokenId) {
+    function mint() public {
         // Generate tokenid
         uint256 random = SourceRandom.getRandomDebug();
-        tokenId = SourceRandom.getSeededRandom(random, _numMinted++);
+        uint256 tokenId = SourceRandom.getSeededRandom(random, _numMinted++);
 
         // Mint Operation
-        MinterCore._mintForFee(speciesId, msg.sender, tokenId);
-
-        emit MintSpecies(speciesId, msg.sender, tokenId);
-
-        return tokenId;
+        MinterCore._mintForFee(msg.sender, tokenId);
     }
 
     /**
      * @dev Create a new type of species and define attributes.
-     * @param speciesId address of associated NFT
-     * @return tokenId minted token id
      */
-    function safeMint(uint256 speciesId) public speciesExists(speciesId) returns (uint256 tokenId) {
+    function safeMint() public {
         // Generate tokenId
         uint256 random = SourceRandom.getRandomDebug();
-        tokenId = SourceRandom.getSeededRandom(random, _numMinted++);
+        uint256 tokenId = SourceRandom.getSeededRandom(random, _numMinted++);
 
         // Mint Operation
-        MinterCore._safeMintForFee(speciesId, msg.sender, tokenId);
-
-        emit MintSpecies(speciesId, msg.sender, tokenId);
-
-        return tokenId;
+        MinterCore._safeMintForFee(msg.sender, tokenId);
     }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function getImplementation() external view returns (address) {
+        return _getImplementation();
+    }
+}
+
+interface IMinterRandom is IERC165Upgradeable {
+    /**
+     * @dev Create a new type of species and define attributes.
+     */
+    function mint() external;
+
+    /**
+     * @dev Create a new type of species and define attributes.
+     */
+    function safeMint() external;
 }
