28c925169fa5de755b13c8eef634abf79edbabdd
HrikB <hrik.bhowal@gmail.com>
2022-06-28 01:24:17 +0400

    MinterAutoId.sol new architecture

--- packages/owlprotocol-contracts/contracts/plugins/Minter/builds/MinterAutoId.sol
+++ packages/owlprotocol-contracts/contracts/plugins/Minter/builds/MinterAutoId.sol
@@ -1,65 +1,118 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '../MinterCore.sol';
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 
-import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import '../MinterCore.sol';
 
 /**
  * @dev Decentralized NFT Minter contract
  *
  */
-contract MinterAutoId is MinterCore {
+contract MinterAutoId is MinterCore, OwnableUpgradeable, UUPSUpgradeable {
     // Track our next tokenId for each species
-    mapping(uint256 => uint256) private nextTokenId;
-
-    // Events
-    event MintSpecies(uint256 indexed speciesId, address to, uint256 tokenId);
+    uint256 nextTokenId;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
+    constructor() initializer {
+        _disableInitializers();
+    }
 
     // Constructor
-    function initialize() public override initializer {
+    function initialize(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) external initializer {
+        __MinterAutoId_init(_admin, _mintFeeToken, _mintFeeAddress, _mintFeeAmount, _nftContractAddr);
+    }
+
+    function proxyInitialize(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) external onlyInitializing {
+        __MinterAutoId_init(_admin, _mintFeeToken, _mintFeeAddress, _mintFeeAmount, _nftContractAddr);
+    }
+
+    function __MinterAutoId_init(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) internal onlyInitializing {
+        __MinterAutoId_init_unchained(_admin);
+        __MinterCore_init(_mintFeeToken, _mintFeeAddress, _mintFeeAmount, _nftContractAddr);
+
+        __Ownable_init();
+    }
+
+    function __MinterAutoId_init_unchained(address _admin) internal onlyInitializing {
         // Register ERC1820 Private Interface
         bytes32 interfaceName = keccak256('OWLProtocol://MinterAutoId');
+        ERC1820ImplementerAuthorizeAll._registerInterfaceForAddress(interfaceName);
         // Register ERC165 Interface
         ERC165Storage._registerInterface(type(IMinterAutoId).interfaceId);
+
+        _transferOwnership(_admin);
     }
+
+    /**
+     * @dev Create a new type of species and define attributes.
+     */
+    function mint() public {
+        MinterCore._mintForFee(msg.sender, nextTokenId++);
+    }
+
+    /**
+     * @dev Create a new type of species and define attributes.
+     */
+    function safeMint() public {
+        MinterCore._safeMintForFee(msg.sender, nextTokenId++);
+    }
+
+    /**
+     * @dev Used to set the starting nextTokenId value.
+     * Used to save situtations where someone mints directly
+     * and we get out of sync.
+     * @param nextTokenId_ next token id to be minted
+     */
+    function setNextTokenId(uint256 nextTokenId_) public onlyOwner {
+        nextTokenId = nextTokenId_;
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function getImplementation() external view returns (address) {
+        return _getImplementation();
+    }
+}
+
+/**
+ * @dev Decentralized NFT Minter contract
+ *
+ */
+interface IMinterAutoId is IERC165Upgradeable {
+    /**
+     * @dev Create a new type of species and define attributes.
+     * @param speciesId address of associated NFT
+     * @return tokenId minted token id
+     */
+    function mint(uint256 speciesId) external returns (uint256 tokenId);
 
     /**
      * @dev Create a new type of species and define attributes.
      * @param speciesId address of associated NFT
      * @return tokenId minted token id
      */
-    function mint(uint256 speciesId) public speciesExists(speciesId) returns (uint256 tokenId) {
-        // Increment tokenId
-        nextTokenId[speciesId]++;
-
-        // Mint Operation
-        MinterCore._mintForFee(speciesId, msg.sender, nextTokenId[speciesId]);
-
-        emit MintSpecies(speciesId, msg.sender, nextTokenId[speciesId]);
-
-        return nextTokenId[speciesId];
-    }
-
-    /**
-     * @dev Create a new type of species and define attributes.
-     * @param speciesId address of associated NFT
-     * @return tokenId minted token id
-     */
-    function safeMint(uint256 speciesId) public speciesExists(speciesId) returns (uint256 tokenId) {
-        // Increment tokenId
-        nextTokenId[speciesId]++;
-
-        // Mint Operation
-        MinterCore._safeMintForFee(speciesId, msg.sender, nextTokenId[speciesId]);
-
-        emit MintSpecies(speciesId, msg.sender, nextTokenId[speciesId]);
-
-        return nextTokenId[speciesId];
-    }
+    function safeMint(uint256 speciesId) external returns (uint256 tokenId);
 
     /**
      * @dev Used to set the starting nextTokenId value.
@@ -68,7 +121,5 @@ contract MinterAutoId is MinterCore {
      * @param speciesId species identifier
      * @param nextTokenId_ next token id to be minted
      */
-    function setNextTokenId(uint256 speciesId, uint256 nextTokenId_) public speciesOwner(speciesId) {
-        nextTokenId[speciesId] = nextTokenId_;
-    }
+    function setNextTokenId(uint256 speciesId, uint256 nextTokenId_) external;
 }
