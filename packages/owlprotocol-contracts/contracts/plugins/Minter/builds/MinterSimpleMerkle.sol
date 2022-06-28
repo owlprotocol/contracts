c2eaf9e818aa894e55e38bf7554f16a27c3289a5
HrikB <hrik.bhowal@gmail.com>
2022-06-28 12:19:29 +0400

    MinterSimpleMerkle.sol required redesign... but brought into the new architecture

--- packages/owlprotocol-contracts/contracts/plugins/Minter/builds/MinterSimpleMerkle.sol
+++ packages/owlprotocol-contracts/contracts/plugins/Minter/builds/MinterSimpleMerkle.sol
@@ -1,66 +1,115 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/introspection/IERC165Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
+
 import '../MinterCore.sol';
-import './IMinterSimpleMerkle.sol';
 
 /**
  * @dev Decentralized NFT Minter contract
  *
  */
-contract MinterSimpleMerkle is MinterCore {
-    // Events
-    event MintSpecies(uint256 indexed speciesId, address to, uint256 tokenId);
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
+contract MinterSimpleMerkle is MinterCore, OwnableUpgradeable, UUPSUpgradeable {
+    // @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {
+        _disableInitializers();
+    }
 
     // Constructor
-    function initialize() public override initializer {
-        // Call parent initializer
-        MinterCore.initialize();
+    function initialize(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) external initializer {}
+
+    function proxyInitialize(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) external onlyInitializing {}
+
+    function __MinterSimpleMerkle_init(
+        address _admin,
+        address _mintFeeToken,
+        address _mintFeeAddress,
+        uint256 _mintFeeAmount,
+        address _nftContractAddr
+    ) internal onlyInitializing {
+        __MinterSimpleMerkle_init_unchained(_admin);
+        __MinterCore_init(_mintFeeToken, _mintFeeAddress, _mintFeeAmount, _nftContractAddr);
+
+        __Ownable_init();
+    }
+
+    function __MinterSimpleMerkle_init_unchained(address _admin) internal onlyInitializing {
         // Register ERC1820 Private Interface
         bytes32 interfaceName = keccak256('OWLProtocol://MinterSimpleMerkle');
         ERC1820ImplementerAuthorizeAll._registerInterfaceForAddress(interfaceName);
         // Register ERC165 Interface
         ERC165Storage._registerInterface(type(IMinterSimpleMerkle).interfaceId);
+
+        _transferOwnership(_admin);
     }
 
     /**
      * @dev Create a new type of species and define attributes.
-     * @param speciesId address of associated NFT
      * @param tokenId minted token id
      */
     function mint(
-        uint256 speciesId,
         uint256 tokenId,
         bytes32 merkleRoot,
         bytes32[] calldata merkleProof
-    ) public speciesExists(speciesId) mintAllowedMerkle(speciesId, merkleRoot, merkleProof) {
-        // Mint Operation
-        MinterCore._mintForFee(speciesId, msg.sender, tokenId);
-
-        emit MintSpecies(speciesId, msg.sender, tokenId);
+    ) public {
+        MinterCore._mintForFee(msg.sender, tokenId);
     }
 
     /**
      * @dev Create a new type of species and define attributes.
-     * @param speciesId address of associated NFT
      * @param tokenId minted token id
      */
     function safeMint(
-        uint256 speciesId,
         uint256 tokenId,
         bytes32 merkleRoot,
         bytes32[] calldata merkleProof
-    ) public speciesExists(speciesId) mintAllowedMerkle(speciesId, merkleRoot, merkleProof) {
-        // Mint Operation
-        MinterCore._safeMintForFee(speciesId, msg.sender, tokenId);
-
-        emit MintSpecies(speciesId, msg.sender, tokenId);
+    ) public {
+        MinterCore._safeMintForFee(msg.sender, tokenId);
     }
 
     function hashKeccakUser() public view returns (bytes32) {
         return keccak256(abi.encode(msg.sender));
     }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function getImplementation() external view returns (address) {
+        return _getImplementation();
+    }
+}
+
+interface IMinterSimpleMerkle is IERC165Upgradeable {
+    /**
+     * @dev
+     * @param tokenId minted token id
+     */
+    function mint(
+        uint256 tokenId,
+        bytes32 merkleRoot,
+        bytes32[] calldata merkleProof
+    ) external;
+
+    /**
+     * @dev
+     * @param tokenId minted token id
+     */
+    function safeMint(
+        uint256 tokenId,
+        bytes32 merkleRoot,
+        bytes32[] calldata merkleProof
+    ) external;
 }
