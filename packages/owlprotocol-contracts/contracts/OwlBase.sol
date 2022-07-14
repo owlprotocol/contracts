646fd0acd1e98c558c46dc5d253fa69f944b7479
Andrew Zhen <43920774+aqzhen@users.noreply.github.com>
2022-07-14 19:54:25 +0400

    setup tests

--- packages/owlprotocol-contracts/contracts/OwlBase.sol
+++ packages/owlprotocol-contracts/contracts/OwlBase.sol
@@ -1,23 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '@opengsn/contracts/src/BaseRelayRecipient.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 
 /**
  * @dev Base for all OWLPROTOCOL contracts
  */
-abstract contract OwlBase is BaseRelayRecipient, UUPSUpgradeable, AccessControlUpgradeable {
+abstract contract OwlBase is UUPSUpgradeable, AccessControlUpgradeable {
     bytes32 internal constant ROUTER_ROLE = keccak256('ROUTER_ROLE');
 
-    function __OwlBase_init(address _admin) internal onlyInitializing {
-        __OwlBase_init_unchained(_admin);
+    function __OwlBase_init(address _admin, address _forwarder) internal onlyInitializing {
+        __OwlBase_init_unchained(_admin, _forwarder);
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+        _grantRole(ROUTER_ROLE, _forwarder);
     }
 
-    function __OwlBase_init_unchained(address _admin) internal onlyInitializing {
-        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
-    }
+    function __OwlBase_init_unchained(address _admin, address _forwarder) internal onlyInitializing {}
 
     /**
      * @notice Must have owner role
@@ -40,8 +40,8 @@ abstract contract OwlBase is BaseRelayRecipient, UUPSUpgradeable, AccessControlU
     /**
      * @notice the following 3 functions are all required for OpenGSN integration
      */
-    function _msgSender() internal view virtual override(BaseRelayRecipient, ContextUpgradeable) returns (address ret) {
-        if (msg.data.length >= 20 && (isTrustedForwarder(msg.sender) || hasRole(ROUTER_ROLE, msg.sender))) {
+    function _msgSender() internal view virtual override returns (address ret) {
+        if (msg.data.length >= 20 && hasRole(ROUTER_ROLE, msg.sender)) {
             // At this point we know that the sender is a trusted forwarder,
             // so we trust that the last bytes of msg.data are the verified sender address.
             // extract sender address from the end of msg.data
@@ -53,21 +53,15 @@ abstract contract OwlBase is BaseRelayRecipient, UUPSUpgradeable, AccessControlU
         }
     }
 
-    function _msgData()
-        internal
-        view
-        virtual
-        override(BaseRelayRecipient, ContextUpgradeable)
-        returns (bytes calldata)
-    {
-        if (msg.data.length >= 20 && (isTrustedForwarder(msg.sender) || hasRole(ROUTER_ROLE, msg.sender))) {
+    function _msgData() internal view virtual override returns (bytes calldata) {
+        if (msg.data.length >= 20 && hasRole(ROUTER_ROLE, msg.sender)) {
             return msg.data[0:msg.data.length - 20];
         } else {
             return msg.data;
         }
     }
 
-    function versionRecipient() external pure virtual override returns (string memory) {
+    function versionRecipient() external pure virtual returns (string memory) {
         return '2.2.6';
     }
 }
---
+++ packages/owlprotocol-contracts/test/plugins/Route Randomizer/RouteRandomizer.test.ts
@@ -0,0 +1,191 @@
+import { ethers, network } from 'hardhat';
+import { time, setCode, mineUpTo } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from 'chai';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import {
+    RouteRandomizer,
+    RouteRandomizer__factory,
+    ERC1167Factory,
+    CrafterMint,
+    CrafterTransfer,
+    Transformer,
+    VRFBeacon,
+    VRFBeacon__factory,
+    VRFCoordinatorV2,
+    FactoryERC721,
+    UpgradeableBeaconInitializable__factory,
+    UpgradeableBeaconInitializable,
+    BeaconProxyInitializable__factory,
+    BeaconProxyInitializable,
+} from '../../../typechain';
+import { pick } from 'lodash';
+import { deployedBytecode as mockDeployedBytecode } from "../../../artifacts/contracts/testing/VRFCoordinatorV2.sol/VRFCoordinatorV2.json"
+
+import { createERC1155, createERC721, deployClone } from '../../utils';
+import { BigNumber, Signer } from 'ethers';
+
+const coordinatorAddr = '0x6168499c0cFfCaCD319c818142124B7A15E857ab';
+const EPOCH_PERIOD = 10;
+const subId = 8101;
+
+enum TokenType {
+    erc20,
+    erc721,
+    erc1155
+}
+
+enum ConsumableType {
+    unaffected,
+    burned,
+    NTime
+}
+
+enum GeneTransformType {
+    none,
+    add,
+    sub,
+    mult,
+    set,
+}
+
+describe('RouteRandomizer.sol', async () => {
+    let VRFBeacon: VRFBeacon;
+    let routeRandomizerImpl: RouteRandomizer;
+    let routeRandomizerArgs: (string | number[] | string[])[];
+
+    let signer1: SignerWithAddress;
+    let burn: SignerWithAddress;
+    let forwarder: SignerWithAddress;
+
+    let crafterInput: FactoryERC721;
+    let transformerInput: FactoryERC721;
+
+    let crafterTransferReward: FactoryERC721;
+    let crafterMintReward: FactoryERC721;
+
+    let outputTokenIds: number[];
+
+    beforeEach(async () => {
+        [signer1, burn, forwarder] = await ethers.getSigners();
+
+        //rinkeby fork
+        await network.provider.request({
+            method: 'hardhat_reset',
+            params: [
+                {
+                    forking: {
+                        jsonRpcUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+                        blockNumber: 11017810,
+                    },
+                },
+            ],
+        });
+
+
+        const VRFBeaconFactory = (await ethers.getContractFactory('VRFBeacon')) as VRFBeacon__factory;
+        VRFBeacon = (await VRFBeaconFactory.deploy(
+            subId,
+            coordinatorAddr,
+            '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc',
+            50000, //gas limit
+            EPOCH_PERIOD,
+        )) as VRFBeacon;
+
+        const ERC1167FactoryFactory = await ethers.getContractFactory("ERC1167Factory");
+        const ERC1167Factory = (await ERC1167FactoryFactory.deploy()) as ERC1167Factory
+
+        //3 crafter contracts
+        const craftableAmount = 15;
+
+        [crafterInput, transformerInput] = await createERC721(3, craftableAmount);
+        [crafterTransferReward] = await createERC721(1, craftableAmount);
+        [crafterMintReward] = await createERC721(1, 0);
+
+        const crafterArgsCommon = [
+            signer1.address,
+            burn.address,
+            craftableAmount,
+            [
+                {
+                    token: TokenType.erc721,
+                    consumableType: ConsumableType.burned,
+                    contractAddr: crafterInput.address,
+                    amounts: [],
+                    tokenIds: [],
+                }
+            ]
+        ]
+
+        outputTokenIds = Array.from(Array(craftableAmount).keys());
+        const crafterOutputCommon = {
+            token: TokenType.erc721,
+            consumableType: ConsumableType.unaffected,
+            amounts: [],
+            tokenIds: outputTokenIds
+        }
+
+        // creating crafter transfer + mint instances
+        const crafterTransferFactory = await ethers.getContractFactory('CrafterTransfer');
+        const crafterTransferImpl = await crafterTransferFactory.deploy();
+
+        const { address: crafterTransferAddress } = await deployClone(crafterTransferImpl, [
+            ...crafterArgsCommon,
+            [{ ...crafterOutputCommon, contractAddr: crafterTransferReward.address, },],
+            forwarder.address, // forwarder addr
+        ], ERC1167Factory);
+
+        const crafterMintFactory = await ethers.getContractFactory("CrafterMint")
+        const crafterMintImpl = await crafterMintFactory.deploy()
+
+        const { address: crafterMintAddress } = await deployClone(crafterMintImpl, [
+            ...crafterArgsCommon, [{ ...crafterOutputCommon, contractAddr: crafterMintReward.address }], forwarder.address
+        ], ERC1167Factory);
+
+        // creating transformer instance 
+        const transformerFactory = await ethers.getContractFactory('Transformer');
+        const transformerImpl = await transformerFactory.deploy();
+
+        const { address: transformerAddress } = await deployClone(transformerImpl, [
+            signer1.address,
+            burn.address,
+            [
+                {
+                    token: TokenType.erc721,
+                    consumableType: ConsumableType.burned,
+                    contractAddr: crafterInput.address, // takes the crafterInput NFT as an input ingredient
+                    amounts: [],
+                    tokenIds: [],
+                },
+            ],
+            [250, 252, 254],
+            [
+                {
+                    geneTransformType: GeneTransformType.add,
+                    value: 1,
+                },
+                {
+                    geneTransformType: GeneTransformType.add,
+                    value: 2,
+                },
+                {
+                    geneTransformType: GeneTransformType.add,
+                    value: 3,
+                },
+            ],
+            transformerInput.address, // transforms this ERC721
+            forwarder.address
+        ], ERC1167Factory);
+
+        //routeRandomizer contract
+        const routeRandomizerFactory = await ethers.getContractFactory("RouteRandomizer") as RouteRandomizer__factory;
+        routeRandomizerImpl = await routeRandomizerFactory.deploy();
+
+        routeRandomizerArgs = [
+            signer1.address,
+            [crafterTransferAddress, crafterMintAddress, transformerAddress],
+            [],
+            [33, 66, 100],
+            forwarder.address
+        ]
+    })
+});
