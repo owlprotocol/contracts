ab4145e913c657e1359b9ee072e97430cc3a17ba
Andrew Zhen <43920774+aqzhen@users.noreply.github.com>
2022-07-15 20:12:29 +0400

    tests working

--- packages/owlprotocol-contracts/test/plugins/RouteRandomizer/RouteRandomizer.test.ts
+++ packages/owlprotocol-contracts/test/plugins/RouteRandomizer/RouteRandomizer.test.ts
@@ -1,6 +1,6 @@
 import { ethers, network } from 'hardhat';
 const { utils } = ethers;
-const { keccak256, toUtf8Bytes } = utils;
+const { keccak256, toUtf8Bytes, defaultAbiCoder } = utils;
 import { time, setCode, mineUpTo } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
@@ -30,6 +30,7 @@ import { createERC1155, createERC721, deployClone, encodeGenesUint256 } from '..
 import { BigNumber, Signer } from 'ethers';
 import { deploy } from '@openzeppelin/hardhat-upgrades/dist/utils';
 import { solidityPack } from 'ethers/lib/utils';
+import { txDataByCompiled } from 'eth-crypto';
 
 const coordinatorAddr = '0x6168499c0cFfCaCD319c818142124B7A15E857ab';
 const EPOCH_PERIOD = 10;
@@ -74,6 +75,8 @@ describe('RouteRandomizer.sol', async () => {
     let outputTokenIds: number[];
 
     let coordinator: VRFCoordinatorV2;
+
+    let transformerImpl: Transformer;
 
     const vals = [1, 1, 1];
     const genes = [250, 252, 254];
@@ -165,18 +168,18 @@ describe('RouteRandomizer.sol', async () => {
             'ERC721OwlAttributes',
         )) as ERC721OwlAttributes__factory;
         const ERC721OwlAttributes = await ERC721OwlAttributesFactory.deploy();
-        const { address } = await deployClone(
+        const { address: attributesAddr } = await deployClone(
             ERC721OwlAttributes,
             [signer1.address, 'n', 's', 'u', forwarder.address],
             ERC1167Factory,
         );
 
-        transformerInput = (await ethers.getContractAt('ERC721OwlAttributes', address)) as ERC721OwlAttributes;
+        transformerInput = (await ethers.getContractAt('ERC721OwlAttributes', attributesAddr)) as ERC721OwlAttributes;
         transformerInput.mint(signer1.address, encodeGenesUint256(vals, genes));
 
         // creating transformer instance
         const transformerFactory = await ethers.getContractFactory('Transformer');
-        const transformerImpl = await transformerFactory.deploy();
+        transformerImpl = (await transformerFactory.deploy()) as Transformer;
 
         const { address: transformerAddress } = await deployClone(
             transformerImpl,
@@ -213,18 +216,21 @@ describe('RouteRandomizer.sol', async () => {
             ERC1167Factory,
         );
 
+        await transformerInput.grantDna(transformerAddress);
+
         //routeRandomizer contract
         const routeRandomizerFactory = (await ethers.getContractFactory('RouteRandomizer')) as RouteRandomizer__factory;
         routeRandomizerImpl = await routeRandomizerFactory.deploy();
 
-        const craftSig = keccak256(toUtf8Bytes('craft(uint96,uint256[][])'));
-        const transformSig = keccak256(toUtf8Bytes('transform(uint256,uint256[][])'));
+        const craftSig = keccak256(toUtf8Bytes('craft(uint96,uint256[][])')).substring(0, 10);
+        const transformSig = keccak256(toUtf8Bytes('transform(uint256,uint256[][])')).substring(0, 10);
 
+        const probabilities = [33, 66, 100];
         routeRandomizerArgs = [
             signer1.address,
             [crafterMintAddr1, crafterMintAddr2, transformerAddress],
             [craftSig, craftSig, transformSig],
-            [33, 66, 100],
+            probabilities,
             VRFBeacon.address,
             forwarder.address,
         ];
@@ -246,6 +252,10 @@ describe('RouteRandomizer.sol', async () => {
         await crafterInput.setApprovalForAll(transformerAddress, true);
 
         await transformerInput.grantDna(routeRandomizerInstAddr);
+
+        await crafterMint1.grantRouter(routeRandomizer.address);
+        await crafterMint2.grantRouter(routeRandomizer.address);
+        await transformer.grantRouter(routeRandomizer.address);
     });
     async function coordinatorRespond(reqId: BigNumber, blockNum: BigNumber) {
         //simulating coordiantor response
@@ -284,8 +294,11 @@ describe('RouteRandomizer.sol', async () => {
     }
 
     it('1 route randomization', async () => {
-        const craftArgs = solidityPack(['uint256', 'uint256[][]'], [1, [[0]]]);
-        const transformArgs = solidityPack(['uint256', 'uint256[][]'], [0, [[0]]]);
+        const craftAmount = 1;
+        const transformTokenId = 0;
+        const inputNFTIds = [[3]];
+        const craftArgs = defaultAbiCoder.encode(['uint96', 'uint256[][]'], [craftAmount, inputNFTIds]);
+        const transformArgs = defaultAbiCoder.encode(['uint256', 'uint256[][]'], [transformTokenId, inputNFTIds]);
         const argsArr = [craftArgs, craftArgs, transformArgs];
         const { requestId, blockNumber } = pick(await routeRandomizer.callStatic.requestRouteRandomize(argsArr), [
             'requestId',
@@ -297,8 +310,64 @@ describe('RouteRandomizer.sol', async () => {
         const { upkeepNeeded } = await routeRandomizer.checkUpkeep('0x');
         expect(upkeepNeeded).to.equal(false);
 
+        await mineUpTo(getEpochBlockNumber(await time.latestBlock()));
         await coordinatorRespond(requestId, blockNumber);
         const { randomness, receipt } = await simulateKeeper(blockNumber);
+
+        const beforeTransform = await transformerInput.getDna(transformTokenId);
+
+        const { events } = receipt;
+        const inputTransferEvent = events ? events[1] : undefined;
+        const randomEvent = events ? events[2] : undefined;
+
+        expect(inputTransferEvent).to.not.equal(undefined);
+        if (inputTransferEvent === undefined) return;
+
+        const { tokenId: inputTokenId } = pick(
+            crafterInput.interface.decodeEventLog('Transfer', inputTransferEvent.data, inputTransferEvent.topics),
+            ['tokenId'],
+        );
+
+        expect(randomEvent).to.not.equal(undefined);
+        if (randomEvent === undefined) return;
+
+        const randContract = await routeRandomizer.getRandomContract(await routeRandomizer.getQueueIndex(), randomness);
+
+        if (randContract.eq(0)) {
+            const { tokenId: tokenIdMinted } = pick(
+                routeRandomizerImpl.interface.decodeEventLog('Transfer', randomEvent.data, randomEvent.topics),
+                ['tokenId'],
+            );
+            //crafter mint 1 is called
+            expect(await crafterMintReward1.ownerOf(tokenIdMinted)).to.equal(signer1.address);
+            expect(await crafterMintReward2.exists(tokenIdMinted)).to.equal(false);
+            expect(await transformerInput.getDna(transformTokenId)).to.equal(beforeTransform);
+        } else if (randContract.eq(1)) {
+            const { tokenId: tokenIdMinted } = pick(
+                routeRandomizerImpl.interface.decodeEventLog('Transfer', randomEvent.data, randomEvent.topics),
+                ['tokenId'],
+            );
+            //crafter mint 2 is called
+            expect(await crafterMintReward1.exists(tokenIdMinted)).to.equal(false);
+            expect(await crafterMintReward2.ownerOf(tokenIdMinted)).to.equal(signer1.address);
+            expect(await transformerInput.getDna(transformTokenId)).to.equal(beforeTransform);
+        } else if (randContract.eq(2)) {
+            //transformer is called
+            expect(await crafterMintReward1.exists(0)).to.equal(false);
+            expect(await crafterMintReward2.exists(0)).to.equal(false);
+
+            const { oldDna, newDna } = pick(
+                transformerImpl.interface.decodeEventLog('Transform', randomEvent.data, randomEvent.topics),
+                ['oldDna', 'newDna'],
+            );
+
+            const newVals = [2, 3, 3];
+            expect(oldDna).to.equal(encodeGenesUint256(vals, genes));
+            expect(newDna).to.equal(encodeGenesUint256(newVals, genes));
+        }
+        expect(await crafterInput.ownerOf(inputTokenId)).to.equal(burn.address); //input ingredient burned
+        // after completion
+        expect((await routeRandomizer.checkUpkeep('0x')).upkeepNeeded).to.equal(false);
     });
 });
 
