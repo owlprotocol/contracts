e3b8250fd31b9a7f1ad020c9d16de850bcacd238
Andrew Zhen <43920774+aqzhen@users.noreply.github.com>
2022-07-18 16:37:40 +0400

    finish lootbox docs

--- packages/owlprotocol-contracts/contracts/plugins/Router/Lootbox.sol
+++ packages/owlprotocol-contracts/contracts/plugins/Router/Lootbox.sol
@@ -18,22 +18,73 @@ import '../../utils/SourceRandom.sol';
 import '../../utils/Probability.sol';
 
 /**
- * @dev Contract module that enables implementation of unlockable lootboxes by routing
- * unlock requests to a number of deployed Crafter contracts. By configuring multiple
- * Crafter contracts that take in the lootbox (typically some ERC721 token) as their sole input
- * and each have different output asset bundles, a developer can route the output of a certain
- * lootbox to a myriad of possible output bundles, effectively designating the inputted token as a
+ * @dev Contract module that enables implementation of unlockable lootboxes by
+ * routing unlock requests to a number of deployed Crafter contracts. By
+ * configuring multiple Crafter contracts that take in the lootbox (typically
+ * some ERC721 token) as their sole input and each have different output asset
+ * bundles, a developer can route the output of a certain lootbox to a myriad of
+ * possible output bundles, effectively designating the inputted token as a
  * lootbox.
  *
- * Configuration for a lootbox requires the developer enter `_crafterContracts`, an address array
- * of deployed Crafter contracts that makes up the pool of potential contracts routed to.
- * The `probabilities` array must be of the same length, as it describes the probability distribution
- * of the random variable that determines a randomly chosen contract.
+ * Configuration for Crafter contracts is designated by two {Ingredient}[]. One
+ * array is the `inputs` and the other is the `outputs`. The contract allows for
+ * the `inputs` to be redeemed for the `outputs`, `craftableAmount` times.
  *
- * A VRFBeacon is deployed and used to inject randomness, while a Chainlink Keeper is used to autonomously
- * check if a random number has been returned by the off-chain VRFBeacon coordinator. Upon receiving a
- * random number, the Keeper then calls the _unlock() function to randomly select a Crafter contract and call
- * its `craft()` function.
+ * ```
+ * struct Ingredient {
+ *     TokenType token;
+ *     ConsumableType consumableType;
+ *     address contractAddr;
+ *     uint256[] amounts;
+ *     uint256[] tokenIds;
+ * }
+ * ```
+ *
+ * Configuration is set in the initializers and cannot be edited once the
+ * contract has been launched Other configurations will require their own
+ * contract to be deployed
+ *
+ * However, `craftableAmount` can be dynamically updated through the {deposit}
+ * and {withdraw} functions which are only accessible to `DEFAULT_ADMIN_ROLE`
+ *
+ * Each Ingredient has a `consumableType` field.* This field is for the `inputs`
+ * elements and ignored by the `outputs` elements. ERC20 and ERC1155 `inputs`
+ * elements can be `unaffected` or `burned`. `unaffected` will check for
+ * ownership/balance while `burned` will send the asset(s) to the `burnAddress`.
+ * ERC721 inputs can be `NTime` or `burned`. `NTime` allows for a specfic
+ * `tokenId` to only be used 'n times', as defined by contract deployer.
+ *
+ * ERC20 `inputs` and `outputs` elements should have one number in the `amounts`
+ * array denoting ERC20 token amount requirement.`tokenIds` should be empty.
+ * NTime consumable type ERC721 inputs should have empty `tokenIds` and
+ * `amounts[0]` equal to `n` - the maximum number of times the input can be
+ * used. Burned ERC721 `inputs` elements should have empty `amounts` and
+ * `tokenIds` array. This contract accepts *all* `tokenId`s from an ERC721
+ * contract as inputs. ERC721 `outputs` elements must have empty `amounts`
+ * array. `tokenIds` array length should be `craftableAmount`. The `tokenIds`
+ * array will contain the `tokenIds` to be transferred out when {craft} is
+ * called. Important to note that output transfers will be from the *end* of the
+ * array since `.pop()` is used.
+ *
+ * ERC1155 `inputs` and `outputs` elements should have the length of `amounts`
+ * and `tokenIds` array be the same. The indices will be linked where each index
+ * denotes how much of each ERC1155 `tokenId` is required.
+ *
+ * This module is used through composition. It can be deployed to create
+ * crafting logic with asset contracts that are already on chain and active;
+ * plug-and-play, so to speak.
+ *
+ * Configuration for a lootbox requires the developer enter `_crafterContracts`,
+ * an address array of deployed Crafter contracts that makes up the pool of
+ * potential contracts routed to.* The `probabilities` array must be of the same
+ * length, as it describes the probability distribution of the random variable
+ * that determines a randomly chosen contract.
+ *
+ * A VRFBeacon is deployed and used to inject randomness, while a Chainlink
+ * Keeper is used to autonomously check if a random number has been returned by
+ * the off-chain VRFBeacon coordinator. Upon receiving a random number, the
+ * Keeper then calls the _unlock() function to randomly select a Crafter
+ * contract and call its `craft()` function.
  */
 
 contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradeable, ERC1155HolderUpgradeable {
@@ -68,7 +119,8 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
      * @dev Create Lootbox instance
      * @param _admin the admin/owner of the contract
      * @param _crafterContracts array of crafterContract address, each with unique recipe
-     * @param _probabilities array of cumulative probabilities associated with using a contract from crafterContracts
+     * @param _probabilities array of cumulative probabilities associated with
+     * using a contract from crafterContracts
      * @param _forwarder address for trusted forwarder for open GSN integration
      */
     function initialize(
@@ -81,6 +133,9 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
         __Lootbox_init(_admin, _crafterContracts, _probabilities, _vrfBeacon, _forwarder);
     }
 
+    /**
+     * @dev Initializes contract through beacon proxy (replaces constructor in proxy pattern)
+     */
     function proxyInitialize(
         address _admin,
         address[] calldata _crafterContracts,
@@ -91,6 +146,10 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
         __Lootbox_init(_admin, _crafterContracts, _probabilities, _vrfBeacon, _forwarder);
     }
 
+    /**
+     * @dev performs validations that `_inputs`  are valid and creates the
+     * configuration
+     */
     function __Lootbox_init(
         address _admin,
         address[] calldata _crafterContracts,
@@ -103,6 +162,10 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
         __Lootbox_init_unchained(_crafterContracts, _probabilities, _vrfBeacon);
     }
 
+    /**
+     * @dev performs validations that `_inputs` and `_outputs` are valid and
+     * creates the configuration
+     */
     function __Lootbox_init_unchained(
         address[] calldata _crafterContracts,
         uint8[] calldata _probabilities,
@@ -122,16 +185,28 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
          Interaction
     **********************/
 
-    function requestUnlock(uint256 lootboxId) external returns (uint256 requestId, uint256 blockNumber) {
+    /**
+     * @dev Makes a request for random number from VRFBeacon, adds unlock
+     * request to queue - specific to lootbox token id
+     * @param lootboxId the token ID of the lootbox asset to be unlocked
+     * @return requestId the id given to the request by the VRFBeacon
+     * @return epochBlockNumber the epoch block number at the time request is made
+     */
+    function requestUnlock(uint256 lootboxId) external returns (uint256 requestId, uint256 epochBlockNumber) {
         uint256 currEntry = lootboxIdToEpochBlock[lootboxId];
         if (currEntry != 0) return (VRFBeacon(vrfBeacon).getRequestId(currEntry), currEntry); //Each lootbox can only be redeemed once
 
-        (requestId, blockNumber) = VRFBeacon(vrfBeacon).requestRandomness();
+        (requestId, epochBlockNumber) = VRFBeacon(vrfBeacon).requestRandomness();
 
-        lootboxIdToEpochBlock[lootboxId] = blockNumber;
+        lootboxIdToEpochBlock[lootboxId] = epochBlockNumber;
         upkeepQueue.push(lootboxId);
     }
 
+    /**
+     * @dev Run by Chainlink Keeper to check whether random number has been returned by VRFBeacon
+     * @return upkeepNeeded whether a random number has been returned
+     * @return performData encoding of random number and queue index
+     */
     function checkUpkeep(
         bytes calldata /* checkData */
     ) external view override returns (bool upkeepNeeded, bytes memory performData) {
@@ -144,6 +219,10 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
         return (false, '0x');
     }
 
+    /**
+     * @dev Run by Chainlink Keeper to perform unlock on a lootbox id requiring upkeep
+     * @param performData encoding of random number and queue index via checkUpkeep
+     */
     function performUpkeep(bytes calldata performData) external override {
         (uint256 randomness, uint256 queueIndexRequest) = abi.decode(performData, (uint256, uint256));
 
@@ -155,6 +234,12 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
         queueIndex++;
     }
 
+    /**
+     * @dev Internal function called by performUpkeep() to call randomly-selected Crafter contract
+     * @param lootboxId the id of the lootbox to unlock
+     * @param randomSeed the random seed generated by the VRFBeacon to be used
+     * to source a random number
+     */
     function _unlock(uint256 lootboxId, uint256 randomSeed) internal {
         //randomly choose the crafter contract to call
         uint256 randomNumber = SourceRandom.getSeededRandom(randomSeed, lootboxId);
@@ -169,7 +254,7 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
 
         address selectedCrafter = crafterContracts[selectedContract];
 
-        // try catch to prevent from reverting. queueIndex will not
+        // low-level call to prevent from reverting. Otherwise, queueIndex will not
         // increment and performUpkeep will be endlessly called,
         // not allowing upkeepQueue to make progress
 
@@ -183,11 +268,24 @@ contract Lootbox is PluginsCore, KeeperCompatibleInterface, ERC721HolderUpgradea
     /**
     Getters
     */
+
+    /**
+     * @notice Used for testing only
+     * @dev Returns epoch block during which requestUnlock was called on a lootbox ID
+     * @param lootboxId the ID of the lootbox to unlock
+     * @return uint256 the epoch block corresponding to lootboxId
+     */
     function getEpochBlock(uint256 lootboxId) public view returns (uint256) {
         return lootboxIdToEpochBlock[lootboxId];
     }
 
-    // used for testing
+    /**
+     * @notice Used for testing only
+     * @dev Returns the randomly selected contract from the contracts array
+     * @param lootboxId the ID of the lootbox to unlock, used as nonce in random number generation
+     * @param randomSeed the random seed taken from checkUpkeep
+     * @return selectedContract the randomly selected contract's index in contracts array
+     */
     function getRandomContract(uint256 lootboxId, uint256 randomSeed) external view returns (uint256 selectedContract) {
         uint256 randomNumber = SourceRandom.getSeededRandom(randomSeed, lootboxId);
         selectedContract = Probability.probabilityDistribution(randomNumber, probabilities);
--- packages/owlprotocol-contracts/contracts/plugins/Transformer/Transformer.sol
+++ packages/owlprotocol-contracts/contracts/plugins/Transformer/Transformer.sol
@@ -14,16 +14,68 @@ import '../../assets/ERC721/ERC721OwlAttributes.sol';
 import './TransformerCore.sol';
 
 /**
- * @dev Contract module that enables transformation of ERC721Owl assets under the same
- * inputs -> outputs logic defined in the Crafter contracts. The Transformer, like Crafter, takes
- * different types of input assets (ERC20, ERC721, ERC1155) in addition to the ERC721Owl to be transformed.
- * However, instead of a new output being transferred or minted to the caller, transformations are made
- * to the existing ERC721Owl's DNA. Logic regarding ingredient consumable type follows that of the Crafter.
+ * @dev Contract module that enables transformation of ERC721Owl assets under
+ * the same inputs -> outputs logic defined in the Crafter contracts. The
+ * Transformer, like Crafter, takes different types of input assets (ERC20,
+ * ERC721, ERC1155) in addition to the ERC721Owl to be transformed. However,
+ * instead of a new output being transferred or minted to the caller,
+ * transformations are made to the existing ERC721Owl's DNA. Logic regarding
+ * ingredient consumable type follows that of the Crafter:
  *
- * Transform configuration is designated by `admin`, `burnAddress`, and an {Ingredient}[] of inputs, as in
- * the Crafter. In addition, it takes an integer array `genes` denoting the start point of each gene within the
- * bit representation of ERC721Owl's DNA. The `modifications` array is of the same length of `genes`, as it describes
- * the modifications that should be made to each gene in the form of a `GeneMod` struct.
+ * Crafting configuration is designated by two {Ingredient}[]. One array is the
+ * `inputs` and the other is the `outputs`. The contract allows for the `inputs`
+ * to be redeemed for the `outputs`, `craftableAmount` times.
+ *
+ * ```
+ * struct Ingredient {
+ *     TokenType token;
+ *     ConsumableType consumableType;
+ *     address contractAddr;
+ *     uint256[] amounts;
+ *     uint256[] tokenIds;
+ * }
+ * ```
+ *
+ * Configuration is set in the initializers and cannot be edited once the
+ * contract has been launched Other configurations will require their own
+ * contract to be deployed
+ *
+ * However, `craftableAmount` can be dynamically updated through the {deposit}
+ * and {withdraw} functions which are only accessible to `DEFAULT_ADMIN_ROLE`
+ *
+ * Each Ingredient has a `consumableType` field.* This field is for the `inputs`
+ * elements and ignored by the `outputs` elements. ERC20 and ERC1155 `inputs`
+ * elements can be `unaffected` or `burned`. `unaffected` will check for
+ * ownership/balance while `burned` will send the asset(s) to the `burnAddress`.
+ * ERC721 inputs can be `NTime` or `burned`. `NTime` allows for a specfic
+ * `tokenId` to only be used 'n times', as defined by contract deployer.
+ *
+ * ERC20 `inputs` and `outputs` elements should have one number in the `amounts`
+ * array denoting ERC20 token amount requirement. `tokenIds` should be empty.
+ * NTime consumable type ERC721 inputs should have empty `tokenIds` and
+ * `amounts[0]` equal to `n` - the maximum number of times the input can be used.
+ * Burned ERC721 `inputs` elements should have empty `amounts` and `tokenIds`
+ * array. This contract accepts *all* `tokenId`s from an ERC721 contract as
+ * inputs. ERC721 `outputs` elements must have empty `amounts` array. `tokenIds`
+ * array length should be `craftableAmount`. The `tokenIds` array will contain
+ * the `tokenIds` to be transferred out when {craft} is called. Important to
+ * note that output transfers will be from the *end* of the array since `.pop()`
+ * is used.
+ *
+ * ERC1155 `inputs` and `outputs` elements should have the length of `amounts`
+ * and `tokenIds` array be the same. The indices will be linked where each index
+ * denotes how much of each ERC1155 `tokenId` is required.
+ *
+ * This module is used through composition. It can be deployed to create
+ * crafting logic with asset contracts that are already on chain and active;
+ * plug-and-play, so to speak.
+ *
+ * Transform configuration is designated by `admin`, `burnAddress`, and an
+ * {Ingredient}[] of inputs, as in the Crafter. In addition, it takes an integer
+ * array `genes` denoting the start point of each gene within the bit
+ * representation of ERC721Owl's DNA. The `modifications` array is of the same
+ * length of `genes`, as it describes the modifications that should be made to
+ * each gene in the form of a `GeneMod` struct.
  *
  * ```
  * struct GeneMod {
@@ -32,16 +84,15 @@ import './TransformerCore.sol';
  * }
  *
  * ```
- * The GeneMod struct refers to a `GeneTransformType`, an enum that can be declared as one of the operations:
- * add, subtract, multiply, divide, or set. The `value` then specifies the amount by which to perform the operation.
  *
- * This configuration is set in the initializers
- * and cannot be edited once the contract has been launched
- * Other configurations will require their own contract to
- * be deployed.
- *
- * Upon successful completion of the `transform()` operation, the ERC721Owl with the passed tokenID will
- * have its DNA modified in-place, never having been transferred out of the caller's possession.
+ * The GeneMod struct refers to a `GeneTransformType`, an enum that can be
+ * declared as one of the operations: add, subtract, multiply, divide, or set.
+ * The `value` then specifies the amount by which to perform the operation.
+ * This configuration is set in the initializers and cannot be edited once the
+ * contract has been launched Other configurations will require their own
+ * contract to be deployed.* Upon successful completion of the `transform()`
+ * operation, the ERC721Owl with the passed tokenID will have its DNA modified
+ * in-place, never having been transferred out of the caller's possession.
  */
 contract Transformer is TransformerCore, ERC721HolderUpgradeable, ERC1155HolderUpgradeable {
     // Specification + ERC165
@@ -73,7 +124,8 @@ contract Transformer is TransformerCore, ERC721HolderUpgradeable, ERC1155HolderU
      * @param _burnAddress Burn address for burn inputs
      * @param _inputs input ingredients for configuration
      * @param _genes array denoting start location of genes within the 256 bit DNA
-     * @param _modifications array denoting the modifications to be made upon each gene after transformation
+     * @param _modifications array denoting the modifications to be made upon
+     * each gene after transformation
      * @param _nftAddr the address of the ERC721Owl contract
      * @param _forwarder trusted forwarder address for open GSN
      */
@@ -90,7 +142,8 @@ contract Transformer is TransformerCore, ERC721HolderUpgradeable, ERC1155HolderU
     }
 
     /**
-     * @dev Initializes contract through beacon proxy (replaces constructor in proxy pattern)
+     * @dev Initializes contract through beacon proxy (replaces constructor in
+     * proxy pattern)
      */
     function proxyInitialize(
         address _admin,
@@ -105,7 +158,8 @@ contract Transformer is TransformerCore, ERC721HolderUpgradeable, ERC1155HolderU
     }
 
     /**
-     * @dev performs validations that `_inputs` and `_outputs` are valid and creates the configuration
+     * @dev performs validations that `_inputs` and `_outputs` are valid and
+     * creates the configuration
      */
     function __Transformer_init(
         address _admin,
@@ -124,7 +178,8 @@ contract Transformer is TransformerCore, ERC721HolderUpgradeable, ERC1155HolderU
     }
 
     /**
-     * @dev performs validations that `_inputs` and `_outputs` are valid and creates the configuration
+     * @dev performs validations that `_inputs` and `_outputs` are valid and
+     * creates the configuration
      */
     function __Transformer_init_unchained(
         address _burnAddress,
@@ -155,7 +210,8 @@ contract Transformer is TransformerCore, ERC721HolderUpgradeable, ERC1155HolderU
 
     /**
      * @dev Used to transform. Consumes inputs and modifies DNA of inputted NFT token.
-     * @notice the transformer instance from which this method is called from must have ERC721OwlAttributes DNA_ROLE
+     * @notice the transformer instance from which this method is called from
+     * must have ERC721OwlAttributes DNA_ROLE
      * @param tokenId ID of NFT token to transform
      * @param _inputERC721Ids Array of pre-approved NFTs for crafting usage.
      */
