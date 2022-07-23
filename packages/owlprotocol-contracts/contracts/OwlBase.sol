a4e016094b0aabd1cfc446eda08c75cbcabd22c3
corbanvilla <corbanvilla@gmail.com>
2022-07-23 19:56:46 +0400

    started more docs

--- packages/owlprotocol-contracts/contracts/OwlBase.sol
+++ packages/owlprotocol-contracts/contracts/OwlBase.sol
@@ -6,31 +6,39 @@ import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol'
 
 /**
  * @dev Base for all OWLPROTOCOL contracts
+ *
+ * Implements several required mechanisms for all OwlProtocol contracts to
+ * utilize:
+ * - OpenGSN support (gasless transactions)
+ * - Consistent contract versioning
+ * - Consistent access control
+ * - UUPS contract upgrade support
  */
 abstract contract OwlBase is UUPSUpgradeable, AccessControlUpgradeable {
     string internal constant __version = 'v0.1';
     bytes32 internal constant ROUTER_ROLE = keccak256('ROUTER_ROLE');
 
+    /**
+     * @dev OwlBase required initialization
+     * @param _admin address to assign owner rights
+     * @param _forwarder OpenGSN forwarder address (if desired).
+     */
     function __OwlBase_init(address _admin, address _forwarder) internal onlyInitializing {
         __OwlBase_init_unchained(_admin, _forwarder);
+    }
 
+    /**
+     * @dev OwlBase unchained initialization.
+     * For future implementation.
+     */
+    function __OwlBase_init_unchained(address _admin, address _forwarder) internal onlyInitializing {
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
         _grantRole(ROUTER_ROLE, _forwarder);
     }
 
-    function __OwlBase_init_unchained(address _admin, address _forwarder) internal onlyInitializing {}
-
-    /**
-     * @notice **REQUIRED FOR GSN**
-     * @dev Return trusted forwarder status.
-     */
-    function isTrustedForwarder(address forwarder) public view returns (bool) {
-        return hasRole(ROUTER_ROLE, forwarder);
-    }
-
     /**
      * @notice Must have owner role
-     * @dev Grants ROUTER_ROLE to {a}
+     * @dev Grants ROUTER_ROLE to {to}
      * @param to address to
      */
     function grantRouter(address to) public onlyRole(DEFAULT_ADMIN_ROLE) {
@@ -38,16 +46,23 @@ abstract contract OwlBase is UUPSUpgradeable, AccessControlUpgradeable {
     }
 
     /**
-     * UUPS functions
+     * @notice Only callable by admins
+     * @dev UUPS function to authorize upgrades
+     * @param newImplementation newImplementation
      */
-    function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 
+    /**
+     * @dev Returns the implementation address.
+     */
     function getImplementation() external view returns (address) {
         return _getImplementation();
     }
 
     /**
      * @notice the following 3 functions are all required for OpenGSN integration
+     * @dev Support for meta transactions
+     * @return ret either msg.sender or user who called transaction through a relayer
      */
     function _msgSender() internal view virtual override returns (address ret) {
         if (msg.data.length >= 20 && hasRole(ROUTER_ROLE, msg.sender)) {
@@ -62,6 +77,10 @@ abstract contract OwlBase is UUPSUpgradeable, AccessControlUpgradeable {
         }
     }
 
+    /**
+     * @dev Support for meta transactions
+     * @return msgData from either msg.sender or from user who called through relayer
+     */
     function _msgData() internal view virtual override returns (bytes calldata) {
         if (msg.data.length >= 20 && hasRole(ROUTER_ROLE, msg.sender)) {
             return msg.data[0:msg.data.length - 20];
@@ -70,11 +89,31 @@ abstract contract OwlBase is UUPSUpgradeable, AccessControlUpgradeable {
         }
     }
 
+    /**
+     * @dev Returns OpenGSN contract version (used for compatibility checks)
+     */
     function versionRecipient() external pure virtual returns (string memory) {
         return '2.2.6';
     }
 
+    // TODO - implement this
     // function version() external pure virtual returns (string memory) {
     //     return __version;
     // }
+
+    /**
+     * @dev Determine is an address a GSN trusted forwarder.
+     * @param forwarder address to query
+     * @return OpenGSN trusted forwardder status
+     */
+    function isTrustedForwarder(address forwarder) public view returns (bool) {
+        return hasRole(ROUTER_ROLE, forwarder);
+    }
+
+    /**
+     * ERC165 support
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == ERC165TAG || super.supportsInterface(interfaceId);
+    }
 }
--- packages/owlprotocol-contracts/contracts/plugins/Minter/builds/MinterAutoId.sol
+++ packages/owlprotocol-contracts/contracts/plugins/Minter/builds/MinterAutoId.sol
@@ -13,6 +13,19 @@ import '../MinterCore.sol';
 /**
  * @dev Decentralized NFT Minter contract
  *
+ * Auto-incrementing NFT minter contracts. Every time `mint` or `safeMint` is
+ * called, the NFT id is automatically generated and minted on the fly. Great
+ * for end-users to interact with without requiring clients to monitor the
+ * blockchain.
+ *
+ * As all Minter contracts interact with existing NFTs, MinterCore expects two
+ * standard functions exposed by the NFT:
+ * - `mint(address to, uint256 tokenId)`
+ * - `safeMint(address to, uint256 tokenId)`
+ *
+ * Additionally, Minter contracts must have required permissions for minting. In
+ * the case that you're using ERC721Owl, you'll do that with
+ * `ERC721Owl.grantMinter(MinterContract)`.
  */
 contract MinterAutoId is MinterCore {
     // Specification + ERC165
@@ -27,7 +40,16 @@ contract MinterAutoId is MinterCore {
         _disableInitializers();
     }
 
-    // Constructor
+    /**
+     * @notice This function is usually called through ERC1167Factory cloning and not directly.
+     * @dev MinterAutoId initialization parameters.
+     * @param _admin user to grant admin privileges
+     * @param _mintFeeToken ERC20 token address to use for minting (ZeroAddress if none)
+     * @param _mintFeeAddress address to transfer minting payments to
+     * @param _mintFeeAmount Number of tokens to charge users (0 if none)
+     * @param _nftContractAddr NFT address to mint
+     * @param _forwarder OpenGSN forwarder address to use
+     */
     function initialize(
         address _admin,
         address _mintFeeToken,
@@ -39,6 +61,16 @@ contract MinterAutoId is MinterCore {
         __MinterAutoId_init(_admin, _mintFeeToken, _mintFeeAddress, _mintFeeAmount, _nftContractAddr, _forwarder);
     }
 
+    /**
+     * @notice This function is usually called through ERC1167Factory cloning and not directly.
+     * @dev MinterAutoId initialization parameters.
+     * @param _admin user to grant admin privileges
+     * @param _mintFeeToken ERC20 token address to use for minting (ZeroAddress if none)
+     * @param _mintFeeAddress address to transfer minting payments to
+     * @param _mintFeeAmount Number of tokens to charge users (0 if none)
+     * @param _nftContractAddr NFT address to mint
+     * @param _forwarder OpenGSN forwarder address to use
+     */
     function proxyInitialize(
         address _admin,
         address _mintFeeToken,
@@ -50,6 +82,16 @@ contract MinterAutoId is MinterCore {
         __MinterAutoId_init(_admin, _mintFeeToken, _mintFeeAddress, _mintFeeAmount, _nftContractAddr, _forwarder);
     }
 
+    /**
+     * @notice This function is never called directly.
+     * @dev MinterAutoId initialization parameters.
+     * @param _admin user to grant admin privileges
+     * @param _mintFeeToken ERC20 token address to use for minting (ZeroAddress if none)
+     * @param _mintFeeAddress address to transfer minting payments to
+     * @param _mintFeeAmount Number of tokens to charge users (0 if none)
+     * @param _nftContractAddr NFT address to mint
+     * @param _forwarder OpenGSN forwarder address to use
+     */
     function __MinterAutoId_init(
         address _admin,
         address _mintFeeToken,
@@ -57,15 +99,19 @@ contract MinterAutoId is MinterCore {
         uint256 _mintFeeAmount,
         address _nftContractAddr,
         address _forwarder
-    ) internal onlyInitializing {
+    ) private onlyInitializing {
         __MinterCore_init(_admin, _mintFeeToken, _mintFeeAddress, _mintFeeAmount, _nftContractAddr, _forwarder);
         __MinterAutoId_init_unchained();
     }
 
+    /**
+     * @dev For future implementations.
+     */
     function __MinterAutoId_init_unchained() internal onlyInitializing {}
 
     /**
-     * @dev Create a new type of species and define attributes.
+     * @dev Mint the next NFT at `nextTokenId`.
+     * @param buyer user to pay for and send the NFT to
      * @return nextTokenId
      */
     function mint(address buyer) public virtual returns (uint256) {
@@ -74,7 +120,8 @@ contract MinterAutoId is MinterCore {
     }
 
     /**
-     * @dev Create a new type of species and define attributes.
+     * @dev Mint the next NFT at `nextTokenId`.
+     * @param buyer user to pay for and send the NFT to
      * @return nextTokenId
      */
     function safeMint(address buyer) public virtual returns (uint256) {
@@ -83,9 +130,8 @@ contract MinterAutoId is MinterCore {
     }
 
     /**
-     * @dev Used to set the starting nextTokenId value.
-     * Used to save situtations where someone mints directly
-     * and we get out of sync.
+     * @dev Used to set the starting nextTokenId value. Used to save situtations
+     * where someone mints directly and we get out of sync.
      * @param nextTokenId_ next token id to be minted
      */
     function setNextTokenId(uint256 nextTokenId_) public onlyRole(DEFAULT_ADMIN_ROLE) {
